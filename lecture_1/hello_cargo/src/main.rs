6eaa6586e72a19d46a358aff8ef721a73600b21e
Scott <hscottvo@gmail.com>
2023-02-26 00:49:09 -0800

    Testing with tuples

--- lecture_1/hello_cargo/src/main.rs
+++ lecture_1/hello_cargo/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    let my_tup: (i32, u64, u8) = (-5, 304, 60);
+    let (x, y, z) = my_tup;
+    println!("x: {}, y: {}, z: {}", x, y, z);
 }
