85e598575287b9bb89b727c4287078d3f90cd6aa
Scott <hscottvo@gmail.com>
2023-03-10 14:34:50 -0800

    Added vector with initialization

--- lecture_2/src/main.rs
+++ lecture_2/src/main.rs
@@ -1,8 +1,14 @@
 fn main() {
+    // basic initialization
     // let mut v: Vec<i32> = Vec::new();
     // v.push(5);
     // v.push(6);
-    let mut v: Vec<i32> = vec![5, 6];
+
+    // easier vector constructor
+    // let mut v: Vec<i32> = vec![5, 6];
+
+    // 8-size with value 4
+    let v: Vec<i32> = vec![4; 8];
 
     for val in &v {
         println!("value: {}", val);
