f5f3eaa745e76c868876ca51053ba14d91b915e0
Scott <hscottvo@gmail.com>
2023-03-24 18:56:03 -0700

    Fixed merge conflicts

--- lecture_2/src/main.rs
+++ lecture_2/src/main.rs
@@ -1,16 +1,14 @@
 fn main() {
-    // basic initialization
-    // let mut v: Vec<i32> = Vec::new();
-    // v.push(5);
-    // v.push(6);
+    let mut v: Vec<i32> = vec![3; 6];
 
-    // easier vector constructor
-    // let mut v: Vec<i32> = vec![5, 6];
+    v.push(5);
+    for i in &mut v {
+        println!("{:}", i);
+    }
 
-    // 8-size with value 4
-    let v: Vec<i32> = vec![4; 8];
+    println!("");
 
-    for val in &v {
-        println!("value: {}", val);
+    for i in &mut v[3..5] {
+        println!("{:}", i);
     }
 }
